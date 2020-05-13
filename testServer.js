db5f7a35a475f335cbc11541beb7b29b1bc2765c
Franco Fontana <fefontana@gmail.com>
2020-05-13 18:19:31 -0300

    Setting Mongo connectio status to response object

--- testServer.js
+++ testServer.js
@@ -5,20 +5,27 @@ const http = require("http");
 const hostname = "0.0.0.0";
 //const port = 3000;
 const port = 8080;
+let connOK = false;
+let connStatus = "";
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
+
+  mongoose
+    .connect("mongodb://172.30.70.75:27017/roc-mongodb", {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    })
+    .then(() => (connOK = true));
+
+  if (connOK) {
+    connStatus = "DB connection successful!!!";
+  } else connStatus = "Connection Error!";
+
   res.end("Hello World!!!! " + "Montevideo - " + "URUGUAY");
 });
-
-mongoose
-  .connect("mongodb://172.30.70.75:27017/roc-mongodb", {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  })
-  .then(() => console.log("DB connection successful!"));
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
