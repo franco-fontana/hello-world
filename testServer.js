6ac11ff09bb2a5f34974a78380edf723764e34a2
Franco Fontana <fefontana@gmail.com>
2020-05-13 17:46:37 -0300

    Adding connection to Mongodb pod

--- testServer.js
+++ testServer.js
@@ -1,3 +1,4 @@
+const mongoose = require("mongoose");
 const http = require("http");
 
 //const hostname = "127.0.0.1";
@@ -10,6 +11,14 @@ const server = http.createServer((req, res) => {
   res.setHeader("Content-Type", "text/plain");
   res.end("Hello World!!!! " + "Montevideo - " + "URUGUAY");
 });
+
+mongoose
+  .connect("mongodb://172.30.70.75:27017/roc-mongodb", {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("DB connection successful!"));
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
