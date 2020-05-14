2f62519a55e2a98e5ca2d264a8d3c2bf1bc6e712
Franco Fontana <fefontana@gmail.com>
2020-05-14 18:39:42 -0300

    Some improvements at connection and when inserting

--- README.md
+++ README.md
@@ -2,7 +2,5 @@
 
 This is my first repository
 
-I'm beginning to use Github.
-Today is thursday.
-
-Now uploading to Github
+It's a Node.js code example to be deployed in an Openshift pod in order to connect to other MongoDB pod using Mongoose.
+After connection is established, a simple document with datetime is inserted.
--- testServer.js
+++ testServer.js
@@ -1,30 +1,39 @@
 const mongoose = require("mongoose");
 const http = require("http");
 
-//const hostname = "127.0.0.1";
-const hostname = "0.0.0.0";
-//const port = 3000;
-const port = 8080;
-let connOK = false;
-let connStatus = "";
+const hostname = "127.0.0.1";
+//const hostname = "0.0.0.0";
+const port = 3000;
+//const port = 8080;
+var connOK = false;
+var connStatus = "";
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
 
   mongoose
-    .connect("mongodb://172.30.70.75:27017/roc-mongodb", {
+    //.connect("mongodb://172.30.70.75:27017/roc-mongodb", {
+    .connect("mongodb://127.0.0.1:27017/tours", {
       useNewUrlParser: true,
       useCreateIndex: true,
       useFindAndModify: false,
+      useUnifiedTopology: true,
     })
-    .then(() => (connOK = true));
+    .then(() => {
+      connOK = true;
+      mongoose.connection.useDb("tours");
+      mongoose.connection.collection("tours").insertOne({
+        name: "Inserting",
+        date: new Date(),
+      });
+      if (connOK) {
+        connStatus = "DB connection successful !!!";
+      } else connStatus = "Connection Error!";
+      res.end("Hello World!!!! " + "Montevideo - " + "URUGUAY " + connStatus);
+    });
 
-  if (connOK) {
-    connStatus = "DB connection successful!!!";
-  } else connStatus = "Connection Error!";
-
-  res.end("Hello World!!!! " + "Montevideo - " + "URUGUAY " + connStatus);
+  if (!connOK) res.end("Not ready yet !!!");
 });
 
 server.listen(port, hostname, () => {
