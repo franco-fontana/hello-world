9ef795c33a1f7e311dfdc94a353e24934c0c2d9f
Franco Fontana <fefontana@gmail.com>
2020-05-19 21:00:52 -0300

    Fix issue favicon.ico executing twice every request

--- testServer.js
+++ testServer.js
@@ -1,39 +1,45 @@
 const mongoose = require("mongoose");
 const http = require("http");
 
-//const hostname = "127.0.0.1";
-const hostname = "0.0.0.0";
-//const port = 3000;
-const port = 8080;
+const hostname = "127.0.0.1";
+//const hostname = "0.0.0.0";
+const port = 3000;
+//const port = 8080;
 var connOK = false;
-var connStatus = "";
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
+  if (req.url != "/favicon.ico") {
+    res.setHeader("Content-Type", "text/plain");
 
-  mongoose
-    .connect("mongodb://172.30.165.24:27017/tours", {
-      //    .connect("mongodb://127.0.0.1:27017/tours", {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-      useUnifiedTopology: true,
-    })
-    .then(() => {
-      connOK = true;
-      mongoose.connection.useDb("tours");
-      mongoose.connection.collection("tours").insertOne({
+    mongoose
+      //    .connect("mongodb://172.30.165.24:27017/tours", {
+      .connect("mongodb://127.0.0.1:27017/tours", {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+      })
+      .then(() => {
+        connOK = true;
+      });
+    //  console.log(req.url);
+    if (!connOK) {
+      res.end("Not ready yet !!!");
+    } else {
+      mongoose.connection.useDb("tours").collection("tours").insertOne({
         name: "Inserting",
         date: new Date(),
       });
-      if (connOK) {
-        connStatus = "DB connection successful !!!";
-      } else connStatus = "Connection Error!";
-      res.end("Hello World!!!! " + "Montevideo - " + "URUGUAY " + connStatus);
-    });
-
-  if (!connOK) res.end("Not ready yet !!!");
+      console.log("Record Inserted . . .");
+      res.statusCode = 200;
+      res.end(
+        "Hello World!!!! " +
+          "Montevideo - " +
+          "URUGUAY " +
+          "***** DB connection successful !!!"
+      );
+    }
+  }
 });
 
 server.listen(port, hostname, () => {
